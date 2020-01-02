6a1e6e1850bd5e1be7eaeacbf8bbe0e602c43cf6
supunkamburugamuva <supun06@gmail.com>
2020-01-02 11:47:17 -0500

    adding to perf

--- src/main/java/org/twister2/perf/spark/InputPartitionJob.java
+++ src/main/java/org/twister2/perf/spark/InputPartitionJob.java
@@ -1,12 +1,15 @@
 package org.twister2.perf.spark;
 
-import org.apache.hadoop.conf.Configuration;
 import org.apache.spark.HashPartitioner;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.PairFunction;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
 import java.math.BigInteger;
@@ -14,12 +17,11 @@ import java.math.BigInteger;
 public class InputPartitionJob {
   public static void main(String[] args) {
     SparkConf conf = new SparkConf().setAppName("terasort");
-    Configuration configuration = new Configuration();
-
     String prefix = args[0] + "/csvData";
     int parallel = Integer.parseInt(args[1]);
     boolean justCSV = Boolean.parseBoolean(args[2]);
     boolean out = Boolean.parseBoolean(args[3]);
+    boolean dataSet = Boolean.parseBoolean(args[4]);
 
     JavaSparkContext sc = new JavaSparkContext(conf);
     JavaRDD<String> input = sc.textFile(prefix, parallel);
@@ -31,19 +33,32 @@ public class InputPartitionJob {
         return new Tuple2<>(new BigInteger(a[0]), Long.parseLong(a[1]));
       }
     });
-    if (justCSV) {
-      if (out) {
-        source.saveAsTextFile(args[0] + "/sparkOut2");
+
+    if (!dataSet) {
+      if (justCSV) {
+        if (out) {
+          source.saveAsTextFile(args[0] + "/sparkOut2");
+        } else {
+          source.saveAsHadoopFile(args[0] + "/sparkOut2", BigInteger.class, Long.class, ByteOutputFormat.class);
+        }
       } else {
-        source.saveAsHadoopFile(args[0] + "/sparkOut2", BigInteger.class, Long.class, ByteOutputFormat.class);
+        if (out) {
+          source.repartitionAndSortWithinPartitions(new HashPartitioner(parallel)).saveAsTextFile(args[0] + "/sparkOut");
+        } else {
+          source.repartitionAndSortWithinPartitions(new HashPartitioner(parallel)).saveAsHadoopFile(
+              args[0] + "/sparkOut", BigInteger.class, Long.class, ByteOutputFormat.class);
+        }
       }
     } else {
-      if (out) {
-        source.repartitionAndSortWithinPartitions(new HashPartitioner(parallel)).saveAsTextFile(args[0] + "/sparkOut");
-      } else {
-        source.repartitionAndSortWithinPartitions(new HashPartitioner(parallel)).saveAsHadoopFile(
-            args[0] + "/sparkOut", BigInteger.class, Long.class, ByteOutputFormat.class);
-      }
+      SparkSession spark = SparkSession
+          .builder()
+          .appName("Java Spark SQL basic example")
+          .config("spark.some.config.option", "some-value")
+          .getOrCreate();
+
+      Dataset<Row> row = spark.createDataset(JavaPairRDD.toRDD(source), Encoders.tuple(Encoders.kryo(BigInteger.class), Encoders.LONG())).toDF("key", "value");
+      Dataset<Row> save = row.repartition(row.col("key")).sortWithinPartitions(row.col("key"));
+      save.write().csv(args[0] + "/sparkOut");
     }
   }
 }
