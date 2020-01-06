1169f529d42c4b86d45cc07bea21e9965dc4636a
Chathura Widanage <chathurawidanage@gmail.com>
2020-01-06 12:03:28 -0500

    Adding spark join job

--- src/main/java/org/twister2/perf/join/spark/JoinJob.java
+++ src/main/java/org/twister2/perf/join/spark/JoinJob.java
@@ -1,57 +1,40 @@
 package org.twister2.perf.join.spark;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.spark.Partitioner;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.Optional;
-import org.twister2.perf.spark.ByteOutputFormat;
-import org.twister2.perf.spark.tera.ByteInputFormat;
-import org.twister2.perf.spark.tera.TeraSortPartitioner;
-import org.twister2.perf.tws.Context;
 import scala.Tuple2;
 
-import java.math.BigInteger;
+import java.util.logging.Logger;
 
 public class JoinJob {
-  public static final String ARG_KEY_SIZE = "keySize";
-  public static final String ARG_DATA_SIZE = "dataSize";
+
+  private final static Logger LOG = Logger.getLogger(JoinJob.class.getName());
 
   public static void main(String[] args) {
+    long t1 = System.currentTimeMillis();
+    LOG.info("Starting join job....");
     SparkConf conf = new SparkConf().setAppName("join");
+    conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+    conf.registerKryoClasses(new Class[]{Integer.class, Long.class});
+
     Configuration configuration = new Configuration();
-
-    double size = Double.parseDouble(args[0]);
-    int keySize = Integer.parseInt(args[2]);
-    int dataSize = Integer.parseInt(args[3]);
-    int tuples = (int) (size * 1024 * 1024 * 1024 / (keySize + dataSize));
-    boolean writeToFile = Boolean.parseBoolean(args[4]);
-
-    configuration.set(Context.ARG_TUPLES, tuples + "");
-    configuration.set(Context.ARG_PARALLEL, args[1]);
-    configuration.set(ARG_KEY_SIZE, args[2]);
-    configuration.set(ARG_DATA_SIZE, args[3]);
-
+    boolean writeToFile = Boolean.parseBoolean(args[0]);
 
     JavaSparkContext sc = new JavaSparkContext(conf);
-    JavaPairRDD<byte[], byte[]> input1 = sc.newAPIHadoopRDD(configuration, ByteInputFormat.class, byte[].class, byte[].class);
-    JavaPairRDD<byte[], byte[]> input2 = sc.newAPIHadoopRDD(configuration, ByteInputFormat.class, byte[].class, byte[].class);
+    JavaPairRDD<Integer, Long> input1 = sc.newAPIHadoopFile(args[1], KeyValueTextInputFormat.class, Text.class, Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString())));
+    JavaPairRDD<Integer, Long> input2 = sc.newAPIHadoopFile(args[2], KeyValueTextInputFormat.class, Text.class, Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString())));
 
-
-    JavaPairRDD<byte[], Tuple2<Optional<byte[]>, byte[]>> joined = input1.rightOuterJoin(input2,
-        new TeraSortPartitioner(
-            input1.partitions().size()
-        ));
+    JavaPairRDD<Integer, Tuple2<Long, Long>> joined = input1.join(input2);
 
     if (writeToFile) {
-      joined.saveAsHadoopFile("out", byte[].class, byte[].class, ByteOutputFormat.class);
-    } else {
-      joined.map(t -> {
-        return new BigInteger(t._1).toString() + " <" + new BigInteger(t._2._2) + "," + (t._2()._1.isPresent() ?
-            new BigInteger(t._2()._1.get()).toString() : "NULL") + ">";
-      }).saveAsTextFile(args[5]);
+      joined.saveAsTextFile(args[3]);
     }
     sc.stop();
+    LOG.info("Stopping join job...");
+    LOG.info("Time taken : " + (System.currentTimeMillis() - t1));
   }
 }
