c7c80a6b5342f66c5e47d636f46d7299823aa001
Chathura Widanage <chathurawidanage@gmail.com>
2020-01-06 16:06:20 -0500

    partitioning

--- src/main/java/org/twister2/perf/join/spark/JoinJob.java
+++ src/main/java/org/twister2/perf/join/spark/JoinJob.java
@@ -3,6 +3,7 @@ package org.twister2.perf.join.spark;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.spark.HashPartitioner;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -15,6 +16,8 @@ public class JoinJob {
   private final static Logger LOG = Logger.getLogger(JoinJob.class.getName());
 
   public static void main(String[] args) {
+    int partitions = Integer.parseInt(args[0]);
+
     LOG.info("Starting join job....");
     SparkConf conf = new SparkConf().setAppName("join");
     conf.set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
@@ -24,15 +27,15 @@ public class JoinJob {
 
     JavaSparkContext sc = new JavaSparkContext(conf);
 
-    JavaPairRDD<Integer, Long> input1 = sc.newAPIHadoopFile(args[0], KeyValueTextInputFormat.class, Text.class,
-        Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString())));
-    JavaPairRDD<Integer, Long> input2 = sc.newAPIHadoopFile(args[1], KeyValueTextInputFormat.class, Text.class,
-        Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString())));
+    JavaPairRDD<Integer, Long> input1 = sc.newAPIHadoopFile(args[1], KeyValueTextInputFormat.class, Text.class,
+        Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString()))).partitionBy(new HashPartitioner(partitions));
+    JavaPairRDD<Integer, Long> input2 = sc.newAPIHadoopFile(args[2], KeyValueTextInputFormat.class, Text.class,
+        Text.class, configuration).mapToPair((t) -> new Tuple2<>(Integer.valueOf(t._1.toString()), Long.valueOf(t._2.toString()))).partitionBy(new HashPartitioner(partitions));
 
     JavaPairRDD<Integer, Tuple2<Long, Long>> joined = input1.join(input2);
 
-    if (args.length > 2) {
-      joined.saveAsTextFile(args[2]);
+    if (args.length > 3) {
+      joined.saveAsTextFile(args[3]);
     }
     sc.stop();
     LOG.info("Stopping join job...");
