c31e1aeef286243c22c73a0f69019c1ba64c410c
Ahmet Uyar <ahmetuyar@gmail.com>
2020-01-30 13:56:21 +0300

    updated params.sh

--- tera/params.sh
+++ tera/params.sh
@@ -1,3 +1,4 @@
+# update all three parameters
 
 export SPARK_HOME="${HOME}/projects/spark-perf/spark-3.0.0-preview2-bin-hadoop2.7"
 
@@ -5,6 +6,5 @@ export SPARK_HOME="${HOME}/projects/spark-perf/spark-3.0.0-preview2-bin-hadoop2.
 # but could not get it work with variable assignment strangely
 # master=$(kubectl cluster-info | head -n 1 | awk '{ print $NF }')
 #K8S_MASTER=k8s://https://ip:port
-export K8S_MASTER=k8s://https://149.165.150.81:6443
 
 export T2_HOME=${HOME}/projects/twister2/twister2-0.5.0-SNAPSHOT
